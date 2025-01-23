22fc5534aaec45ac6643fd0998bed0974723ebe2
Igor Nikiforov <inikiforovj7@icloud.com>
2025-01-23 12:00:18 +0500

    Initial commit

---
+++ InvestCrypto/InvestCrypto/InvestCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvestCryptoApp.swift
+//  InvestCrypto
+//
+//  Created by Игорь Никифоров on 23.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct InvestCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
