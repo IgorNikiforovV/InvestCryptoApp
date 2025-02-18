56e6ebee5a1091e6a754364ee27fe8909f328c76
Igor Nikiforov <inikiforovj7@icloud.com>
2025-02-18 11:04:36 +0500

    Task14 Renovated PortfolioView

--- InvestCrypto/InvestCrypto/Core/Home/Views/PortfolioView.swift
+++ InvestCrypto/InvestCrypto/Core/Home/Views/PortfolioView.swift
@@ -10,19 +10,17 @@ import SwiftUI
 struct PortfolioView: View {
     
     @EnvironmentObject private var vm: HomeViewModel
+    @State private var selectedCoin: CoinModel? = nil
+    @State private var quantityText: String = ""
 
     var body: some View {
         NavigationView {
             ScrollView {
                 VStack(alignment: .leading, spacing: 0) {
                     SearchBarView(searchText: $vm.searchText)
-
-                    ScrollView(.horizontal, showsIndicators: true) {
-                        LazyHStack(spacing: 10) {
-                            ForEach(vm.allCoins) { coin in
-                                Text(coin.symbol.uppercased())
-                            }
-                        }
+                    coinLogoList
+                    if selectedCoin != nil {
+                        portfolioInputSection
                     }
                 }
             }
@@ -36,6 +34,67 @@ struct PortfolioView: View {
     }
 }
 
+extension PortfolioView {
+    private var coinLogoList: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            LazyHStack(spacing: 10) {
+                ForEach(vm.allCoins) { coin in
+                    CoinLogoView(coin: coin)
+                        .frame(width: 75)
+                        .padding(4)
+                        .onTapGesture {
+                            withAnimation(.easeIn) {
+                                selectedCoin = coin
+                            }
+                        }
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(selectedCoin?.id == coin.id ? Color.green : Color.clear, lineWidth: 1)
+                        )
+                }
+            }
+            .frame(height: 120)
+            .padding(.leading)
+        }
+    }
+
+    private func getCurrentValue() -> Double {
+        if let quantity = Double(quantityText) {
+            quantity * (selectedCoin?.currentPrice ?? 0)
+        } else {
+            0
+        }
+    }
+
+    private var portfolioInputSection: some View {
+        VStack(spacing: 20) {
+            HStack {
+                Text("Current price of \(selectedCoin?.symbol.uppercased() ?? ""):")
+                Spacer()
+                Text(selectedCoin?.currentPrice.asCurrencyWith6Decimals() ?? "")
+            }
+            Divider()
+            HStack {
+                Text("Amount holding:")
+                Spacer()
+                TextField("Ex: 1.4", text: $quantityText)
+                    .multilineTextAlignment(.trailing)
+                    .keyboardType(.decimalPad)
+            }
+            Divider()
+            HStack {
+                Text("Current value:")
+                Spacer()
+                Text(getCurrentValue().asCurrencyWith2Decimals())
+            }
+        }
+        .animation(.none)
+        .padding()
+        .font(.headline)
+    }
+}
+
 #Preview {
     PortfolioView()
+        .environmentObject(HomeViewModel())
 }
