bffb0cc37003f299affc0fb5a060bb06d0b42a4f
Igor Nikiforov <inikiforovj7@icloud.com>
2025-03-18 10:43:53 +0500

    Task21 Added ToolbarItem for DetailView

--- InvestCrypto/InvestCrypto/Core/Detail/Views/DetailView.swift
+++ InvestCrypto/InvestCrypto/Core/Detail/Views/DetailView.swift
@@ -46,10 +46,25 @@ struct DetailView: View {
             .padding()
         }
         .navigationTitle(vm.coin.name)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                navigationBarTrailingItems
+            }
+        }
     }
 }
 
 extension DetailView {
+    private var navigationBarTrailingItems: some View {
+        HStack {
+            Text(vm.coin.symbol.uppercased())
+                .font(.headline)
+                .foregroundStyle(Color.theme.secondaryText)
+            CoinImageView(coin: vm.coin)
+                .frame(width: 25, height: 25)
+        }
+    }
+
     private var overviewTitle: some View {
         Text("Overview")
             .font(.title)
