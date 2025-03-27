f5e8e25448f54345d3a35c3f3b8ff123dc77e234
Igor Nikiforov <inikiforovj7@icloud.com>
2025-03-27 11:15:04 +0500

    Task23 Finished SettingsView

--- InvestCrypto/InvestCrypto/Core/Settings/Views/SettingsView.swift
+++ InvestCrypto/InvestCrypto/Core/Settings/Views/SettingsView.swift
@@ -12,15 +12,19 @@ struct SettingsView: View {
     let youtubeURL = URL(string: "https://www.youtube.com/c/swiftfullthinking")!
     let coffeeURL = URL(string: "https://bymeacoffee.com/nicksarno")!
     let coingeckoURL = URL(string: "https://coingecko.com")!
+    let personalURL = URL(string: "https://nicksarno.com")!
+
 
     var body: some View {
         NavigationView {
             List {
-                Section(header: Text("header")) {
-                    Text("Hi")
-                    Text("Hi")
-                }
+                swiftfulThinkingSection
+                coinGeckoSection
+                developerSection
+                applicationSection
             }
+            .font(.headline)
+            .accentColor(.blue)
             .listStyle(.grouped)
             .navigationTitle("Settings")
             .toolbar {
@@ -28,6 +32,70 @@ struct SettingsView: View {
                     XMarkButton()
                 }
             }
+        }
+    }
+}
+
+extension SettingsView {
+    private var swiftfulThinkingSection: some View {
+        Section(header: Text("Swiftful Thinking")) {
+            VStack(alignment: .leading) {
+                Image("logo")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                Text("This app eas made by following the Swiftful Thinking YouTube channel. It uses MVVM Architecture, Combine and CoreData!")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.theme.accent)
+            }
+            .padding(.vertical)
+            Link("Subscribe on Youtube 🤠", destination: youtubeURL)
+            Link("Support his coffee addiction ☕️", destination: youtubeURL)
+        }
+    }
+
+    private var coinGeckoSection: some View {
+        Section(header: Text("CoinGecko")) {
+            VStack(alignment: .leading) {
+                Image("coingecko")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                Text("The cryptocurrency data that is used in this app comes from a free API from Coingecko! Prices may be slightly delayed.")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.theme.accent)
+            }
+            .padding(.vertical)
+            Link("Visit CoinGecko 💰", destination: coingeckoURL)
+        }
+    }
+
+    private var developerSection: some View {
+        Section(header: Text("Developer")) {
+            VStack(alignment: .leading) {
+                Image("logo")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                Text("This app was developed by Nick Sarno. It uses SwiftUI and is written 100% in Swift. The project is open source and can be found on GitHub! It has bnefites from multi-threading, publishers/subscribers, and data persistence.")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.theme.accent)
+            }
+            .padding(.vertical)
+            Link("Visit Website 🧑‍💻", destination: personalURL)
+        }
+    }
+
+    private var applicationSection: some View {
+        Section(header: Text("Application")) {
+            Link("Terms of Service", destination: defaultURL)
+            Link("Privacy Policy", destination: defaultURL)
+            Link("Company Website", destination: defaultURL)
+            Link("Learn More", destination: defaultURL)
         }
     }
 }
