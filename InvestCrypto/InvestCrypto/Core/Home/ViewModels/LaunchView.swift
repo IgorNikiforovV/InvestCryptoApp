1332aa648cdacca04030a86a44f40b5932dc0787
Igor Nikiforov <inikiforovj7@icloud.com>
2025-04-01 11:13:11 +0500

    Task 24 Added Launch Screen.storyboard

---
+++ InvestCrypto/InvestCrypto/Core/Home/ViewModels/LaunchView.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchView.swift
+//  InvestCrypto
+//
+//  Created by Игорь Никифоров on 01.04.2025.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LaunchView()
+}
